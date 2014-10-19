3bfc3a97f5143df8fc31381590a24778dd326c94
Trinh Van Dat <dattv.apt@gmail.com>
2014-10-19 23:48:33 +0700

    Commit fist

---
+++ DAL/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLBH.DAL
+{
+    /// <summary>
+    /// Lớp sử dụng để thực hiện kết nối với CSDL.
+    /// </summary>
+    class Database 
+    {
+    }
+}
